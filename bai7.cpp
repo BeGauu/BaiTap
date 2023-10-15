ec669d1e566e85ac14a2328648cb045dcd2848d0
BeGauu <kimtaitranb278@gmail.com>
2023-10-15 08:37:03 +0700

    bai7

---
+++ bai7.cpp
@@ -0,0 +1,18 @@
+ #include <stdio.h>
+void hoanvi(int &x, int &y);
+ int main()
+ {int a, b;
+      printf("Nhap a="); 
+	  scanf("%d",&a);
+      printf("\nNhap b="); 
+	  scanf("%d",&b);
+               hoanvi(a,b); 
+   printf( "Gia tri a hoanvi: %d\n", a ); 
+      printf( "Gia tri b hoanvi: %d\n", b );
+      return 0;
+
+ }
+void hoanvi(int &x, int &y )
+ { int tam;
+        tam=x;    x=y;     y=tam;
+}
