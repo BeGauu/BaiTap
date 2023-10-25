d20a54fcbe38871eb7cf8f5b37880c8f9c599e82
BeGauu <147892409+BeGauu@users.noreply.github.com>
2023-10-25 16:26:02 +0700

    Update mang2chieu.cpp

--- mang2chieu.cpp
+++ mang2chieu.cpp
@@ -1,4 +1,5 @@
 #include "stdio.h"
+
 int a[10][10];
 int m, n;
 
@@ -37,9 +38,12 @@ void xuat(int a[10][10], int m, int n)
 int max(int [10][10], int m, int n)
 {
 	int max = a[0][0];
-	for(int i=0; i<m; i++){
-		for(int j=0; j<n; j++){
-			if(max<a[i][j]){
+	for(int i=0; i<m; i++)
+	{
+		for(int j=0; j<n; j++)
+		{
+			if(max<a[i][j])
+			{
 				max = a[i][j];
 			}
 		}
@@ -61,14 +65,18 @@ int min(int a[10][10], int m, int n)
 	}
 	return min;
 }
-int timkiem(int a[10][10], int m, int n){
+int timkiem(int a[10][10], int m, int n)
+{
 	int x;
 	int count=0;
   printf("\n Nhap gia tri x can tim: ");
   scanf("%d", &x);
-  for (int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++) {
-      if (a[i][j] == x) {
+  for (int i = 0; i < m; i++) 
+  {
+    for (int j = 0; j < n; j++) 
+	{
+      if (a[i][j] == x) 
+	  {
         printf("x o vi tri  (%d, %d)\n", i, j);
         count++;
       }
@@ -77,28 +85,64 @@ int timkiem(int a[10][10], int m, int n){
  if (count==0){printf ("Khong tim thay x");
  }
 }
-void sapxep(int a[10][10], int m, int n){
+
+void yenngua(int a[10][10], int m, int n) {
+  int min_in_row[m], max_in_col[n];
+  for (int i = 0; i < m; i++) {
+    min_in_row[i] = a[i][0];
+    for (int j = 1; j < n; j++) {
+      if (a[i][j] < min_in_row[i]) {
+        min_in_row[i] = a[i][j];
+      }
+    }
+  }
+
+  for (int j = 0; j < n; j++) {
+    max_in_col[j] = a[0][j];
+    for (int i = 1; i < m; i++) {
+      if (a[i][j] > max_in_col[j]) {
+        max_in_col[j] = a[i][j];
+      }
+    }
+  }
+
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      if (a[i][j] == min_in_row[i] && a[i][j] == max_in_col[j]) {
+        printf("\n [%d][%d] la diem yen ngua\n", i, j);
+      }
+    }
+  }
+}
+
+
+void sapxep(int a[10][10], int m, int n)
+{
 	int k = m*n;
 	for(int i=0; i<k-1; i++)
 	{
 		for(int j=i+1; j<k; j++)
 		{ if(a[i/n][i%n]>a[j/n][j%n])
-		       {
-				int temp  = a[i/n][i%n];
-				a[i/n][i%n] = a[j/n][j%n];
-				a[j/n][j%n] = temp;
-			   }  
+		   {
+			  int temp  = a[i/n][i%n];
+			  a[i/n][i%n] = a[j/n][j%n];
+			  a[j/n][j%n] = temp;
+		   }  
 		}
 	}
 	
 }
-int tongduongcheo(int a[10][10], int m,int n) {
+int tongduongcheo(int a[10][10], int m,int n) 
+{
   int tong = 0;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-            if (i == j) {
-        tong += a[i][j];
-      }
+  for (int i = 0; i < n; i++) 
+  {
+    for (int j = 0; j < n; j++) 
+	{
+       if (i == j)
+		{
+           tong += a[i][j];
+        }
     }
   } return tong;
 }
@@ -106,24 +150,29 @@ int tongduongcheo(int a[10][10], int m,int n) {
 int demsochan(int a[10][10], int m, int n) 
 {  
   int dem = 0;
-  for (int i = n - 1; i >= 0; i--) {
-    for (int j = 0; j < i; j++) {
-      
-      if (a[i][j] % 2 == 0) {
+  for (int i = n - 1; i >= 0; i--) 
+  {
+    for (int j = 0; j < i; j++) 
+	{
+      if (a[i][j] % 2 == 0) 
+	  {
         dem++;
       }
     }
   }
  return dem;
 }
+
 bool kiemtra(int n)
-{ int i;
- if (n<2)
- return false;
+{ 
+   int i;
+   if (n<2)
+  return false;
  for (i=2; i<=n/2;i++ )
     if (n%i==0) return false;
     return true;
 }
+
 int snt (int a[10][10], int m, int n)
 {  int c=0, tong=0;
      for (int i=0; i<m; i++)
@@ -134,6 +183,7 @@ int snt (int a[10][10], int m, int n)
      }printf ("\n So nguyen to trong ma tran : %d \n Tong so nguyen to :%d",c,tong);
 
 }
+
 void sole (int a[10][10], int m, int n) 
 {
   for (int i = m - 1; i >= 0; i--) 
@@ -147,16 +197,20 @@ void sole (int a[10][10], int m, int n)
     }
   }
 }
+
 int main()
 {
 	nhap(a, m, n);
 	xuat(a, m, n);
 	printf ("so chan nam trong tam giac tren cua duong cheo chinh %d \n",demsochan(a,m,n));
-	printf("Tong cac phan tu tren duong cheo chinh : %d\n", tongduongcheo(a, m,n));
+	printf("Tong cac phan tu tren duong cheo chinh : %d", tongduongcheo(a, m,n));
 	snt(a,m,n);
 	sole(a,m,n);
-	printf("Gia tri lon nhat = %d", max(a, m, n));
+	printf("\nGia tri lon nhat = %d", max(a, m, n));
 	printf("\nGia tri nho nhat = %d", min(a, m, n));
 	timkiem(a, m, n);
+	yenngua(a,m,n);
+	printf ("\n Ma tran sau khi sap xep ");
+	sapxep(a,m,n);
+	xuat(a,m,n);
 }
-
