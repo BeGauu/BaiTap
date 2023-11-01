3f28644da5a0833e918ab70185d8e992850272b2
BeGauu <147892409+BeGauu@users.noreply.github.com>
2023-11-01 10:21:25 +0700

    Update baichuoi.cpp

--- baichuoi.cpp
+++ baichuoi.cpp
@@ -73,6 +73,48 @@ int demkitu(char *s1, char *s2)
    }printf ("\nSo ky tu tu: %d",dem);
    
 }
+/* int demtu(char *s1) 
+ {
+  int demtu = 1;
+  strlwr(s1);
+  for (int i = 0; i < strlwr(s1); i++) 
+  {
+    if (s1[i] == ' ') 
+      demtu++;
+  }
+  printf("\nSo tu: %d", demtu);
+  return demtu;
+}*/
+
+void noichuoi(char *s1, char *s2) {
+  int a1 = strlen(s1);
+  int a2 = strlen(s2);
+
+  for (int i = 0; i < a2; i++) {
+    s1[a1 + i] = s2[i];
+  }
+}
+
+int doixung(char *s)
+{  int i,mon=1;
+   for(i=0; i<strlen(s)/2;i++)
+   { if (s[i] != s[strlen(s)-i-1])
+      {  mon=0;
+         break;
+      }
+   }
+   if (mon==1)
+   printf ("\nchuoi: %s doi xung",s);
+   else
+   printf ("\nChuoi khong doi xung",s);
+}
+void inchuoidaonguoc(char *s) {
+  int  n= strlen(s);
+
+  for (int i = n - 1; i >= 0; i--) {
+    printf("%c", s[i]);
+  }
+}
 
 
 int main()
@@ -80,9 +122,16 @@ int main()
    nhap();
    xuat();
    xoakhtrang(s1);xoakhtrang(s2);
+   printf ("\nChuoi sau khi in hoa:");
    inhoa(s1);inhoa(s2);
    xuat();
-   printf("\nSo lan xuat hien cua chuoi S2 l‡: %d\n", demsolanxuathien(s1, s2));
+   printf ("\nChuoi sau khi dao nguoc:"); 
+   inchuoidaonguoc(s1);
+   doixung(s1);
+   printf("\nSo lan xuat hien cua chuoi S2 l√†: %d\n", demsolanxuathien(s1, s2));
    demkitu(s1);
+   //demtu(s1);
+   noichuoi(s1, s2);
+   printf("\nChuoi sau khi noi: %s\n", s1);
    getch();
 }
