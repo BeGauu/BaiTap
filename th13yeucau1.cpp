566e813846d89838ed87f46f6eff9acf0e7f74fb
BeGauu <147892409+BeGauu@users.noreply.github.com>
2023-12-29 07:19:48 +0700

    Add files via upload

---
+++ th13yeucau1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int fibonacci(int n)
+{
+	if (n<0)
+	{
+		return -1;
+	} else if (n == 0 || n == 1)
+	    {
+	    	return n;
+	    } else {
+	    	return fibonacci(n-1)+ fibonacci(n-2);
+	    }
+}
+int main ()
+{
+	printf ("10 so dau tien cua day so Fibonacci: \n");
+	for (int i=0; i<10; i++)
+	   {
+	   	printf("%d   ",fibonacci(i));
+	   }
+}
+
