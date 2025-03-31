d40136f047191d7d2ec96f8187af96098fad962f
BeGauu <147892409+BeGauu@users.noreply.github.com>
2025-03-31 08:15:20 +0700

    Update script.js

--- banTrangsuc/js/script.js
+++ banTrangsuc/js/script.js
@@ -1,1 +1,14 @@
+const bar = document.querySelector('#bar');
+const close = document.querySelector('#close');
+const nav = document.querySelector('#menu');
 
+if (bar && nav) {
+    bar.addEventListener('click', () => {
+        nav.classList.toggle('active'); // Dùng toggle để mở/đóng menu
+    });
+}
+if (close && nav) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active'); // Dùng toggle để mở/đóng menu
+    });
+}
