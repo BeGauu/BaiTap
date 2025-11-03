b9048ada6bd092a10efdcb0fc7f877d4cc5d81d4
BeGauu <147892409+BeGauu@users.noreply.github.com>
2025-11-03 15:50:35 +0700

    Add files via upload

---
+++ C#/bai394041/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai_39_40_41
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            LopSn sn = new LopSn();
+
+            sn.nhap_mang();
+            sn.ktsoam();
+            sn.tong_so_am();
+            sn.tong_so_chan();
+        }
+    }
+    
+}
